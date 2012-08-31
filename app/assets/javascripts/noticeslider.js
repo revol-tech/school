faf4942e470ceff64682f349814ff5f15c387c38
revol-tech <revolt.techno@gmail.com>
2012-08-31 16:49:47 +0545

    flashnews completed

--- app/assets/javascripts/noticeslider.js
+++ app/assets/javascripts/noticeslider.js
@@ -2,13 +2,13 @@ $(function(){
 	$('#slider').bxSlider({
 							mode		:'horizontal',
 							infiniteLoop:'true',
-							speed		:50000,
+							speed		:10000,
 							auto		:true,
 							autoStart	:true,
 							autoHover	:true,
 							autoDirection:'next',
 							//autoDirection:'next',
-							pause		:10,
+							pause		:0,
 							easing: 'linear',
 							autoHover: true,
 							displaySlideQty: 3
